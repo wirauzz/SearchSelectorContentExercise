861aa9ad316cb45f03f8c579e8725743f71934b2
wirauzz <wramirez995@gmail.com>
2022-02-03 16:23:43 -0400

    Small Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SongItemComponent } from './components/song-item/song-item.component';
-import { AudiobookItemComponent } from './components/audiobook-item/audiobook-item.component';
-import { SongVideoItemComponent } from './components/song-video-item/song-video-item.component';
 import { CollectionListComponent } from './components/collection-list/collection-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SongItemComponent,
-    AudiobookItemComponent,
-    SongVideoItemComponent,
     CollectionListComponent
   ],
   imports: [
--- src/app/models/audiobook.ts
+++
@@ -1,9 +0,0 @@
-export class audiobook{
-    artworkUrl100: string;
-    trackName: string;
-    artistName: string;
-    collectionName: string;
-    trackTimeMillis: number;
-    trackPrice: number;
-    previewUrl: string;
-}
